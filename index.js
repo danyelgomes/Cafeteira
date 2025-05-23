cf4d50f97e3e730e4daad86976eed9d5a3d483e0
Danyel Gomes <danyeldev1@gmail.com>
2025-05-23 20:46:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-let menu = document.querySelector('header nav')
-let icon = document.getElementById('icon')
-
-icon.addEventListener('click', function() {
-    menu.classList.toggle('aberto')
-})
+document.getElementById('menu')
